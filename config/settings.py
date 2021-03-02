eb30c7923a2160876786e755c5580a42d32cfab6
mjhuh263 <mjhuh263@gmail.coom>
2021-03-02 18:32:42 +0900

    DELETE: User App

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
-    'user'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
