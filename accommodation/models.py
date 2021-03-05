0056a96dee24b80ed9db688a0285347e9a301902
mjhuh263 <mjhuh263@gmail.coom>
2021-03-05 14:20:36 +0900

    MODIFY: user, accommodation, flight, order models.py

--- accommodation/models.py
+++ accommodation/models.py
@@ -5,28 +5,21 @@ class Accommodation(models.Model):
     description = models.CharField(max_length=1000, null=True)
     rate        = models.DecimalField(max_digits=5, decimal_places=2, null=True)
     content     = models.TextField(null=True)
-    is_for_sale = models.BooleanField(default=1)
-    address     = models.ForeignKey('Address', on_delete=models.RESTRICT)
-    host        = models.ForeignKey('Host', on_delete=models.PROTECT)
-    city        = models.ForeignKey('City', on_delete=models.PROTECT)
+    address     = models.ForeignKey('Address', on_delete=models.CASCADE)
+    host        = models.ForeignKey('Host', on_delete=models.CASCADE)
+    city        = models.ForeignKey('City', on_delete=models.CASCADE)
+    category    = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'accommodations'
 
 class AccommodationImage(models.Model):
     image_url     = models.URLField(max_length=4000) 
-    is_main       = models.BooleanField(default=0)
     accommodation = models.ForeignKey('Accommodation', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'accommodation_images'
 
-class AccommodationKeyword(models.Model):
-    category      = models.ForeignKey('Category', on_delete=models.CASCADE)
-    accommodation = models.ForeignKey('Accommodation', on_delete=models.CASCADE)
-    
-    class Meta:
-        db_table = 'accommodations_keywords'
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -35,8 +28,8 @@ class Category(models.Model):
         db_table = 'categories'
 
 class Address(models.Model):
-    latitude  = models.DecimalField(max_digits=10, decimal_places=6)
-    longitude = models.DecimalField(max_digits=10, decimal_places=6)
+    latitude  = models.DecimalField(max_digits=30, decimal_places=15)
+    longitude = models.DecimalField(max_digits=30, decimal_places=15)
     name      = models.CharField(max_length=300)
 
     class Meta:
@@ -58,7 +51,6 @@ class Host(models.Model):
 class Room(models.Model):
     name             = models.CharField(max_length=300)
     price            = models.DecimalField(max_digits=30, decimal_places=2)
-    is_for_sale      = models.BooleanField(default=1)
     maximum_capacity = models.IntegerField()
     basic_capacity   = models.IntegerField(default=1) 
     extra_price      = models.DecimalField(max_digits=30, decimal_places=2, default=0)
@@ -76,17 +68,17 @@ class RoomOption(models.Model):
         db_table = 'rooms_options'
 
 class Review(models.Model):
-    content          = models.CharField(max_length=1000, null=True)
-    image_url        = models.URLField(max_length=4000, null=True)
-    location_rate    = models.DecimalField(max_digits=5, decimal_places=2)
-    sanitary_rate    = models.DecimalField(max_digits=5, decimal_places=2)
-    facility_rate    = models.DecimalField(max_digits=5, decimal_places=2)
-    hospitality_rate = models.DecimalField(max_digits=5, decimal_places=2)
-    total_rate       = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    created_at       = models.DateTimeField(auto_now_add=True)
-    updated_at       = models.DateTimeField(auto_now=True)
-    user             = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
-    accommodation    = models.ForeignKey('Accommodation', on_delete=models.CASCADE)
+    content                = models.CharField(max_length=1000, null=True)
+    image_url              = models.URLField(max_length=4000, null=True)
+    location_rate          = models.DecimalField(max_digits=5, decimal_places=2)
+    sanitary_rate          = models.DecimalField(max_digits=5, decimal_places=2)
+    facility_rate          = models.DecimalField(max_digits=5, decimal_places=2)
+    hospitality_rate       = models.DecimalField(max_digits=5, decimal_places=2)
+    total_rate             = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    created_at             = models.DateTimeField(auto_now_add=True)
+    updated_at             = models.DateTimeField(auto_now=True)
+    user                   = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
+    accommodation          = models.ForeignKey('Accommodation', on_delete=models.CASCADE)
     accommodation_order_id = models.ForeignKey('order.AccommodationOrder', on_delete=models.SET_NULL, null=True)
 
     class Meta:
@@ -98,11 +90,12 @@ class Option(models.Model):
     class Meta:
         db_table = 'options'
 
-class RoomFluctuation(models.Model):
-    date      = models.DateField()
-    room      = models.ForeignKey('Room', on_delete=models.CASCADE)
-    sale_rate = models.DecimalField(max_digits=5, decimal_places=2)
+class UnavailableDate(models.Model):
+    start_date    = models.DateField()
+    end_date      = models.DateField()
+    room          = models.ForeignKey('Room', on_delete=models.CASCADE)
+    accommodation = models.ForeignKey('Accommodation', on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'room_fluctuations'
+        db_table = 'unavailable_dates'
 
