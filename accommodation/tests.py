322561552883f6256b6b6a98b68c808842852252
phin09 <sy6kang@gmail.com>
2021-03-07 13:26:11 +0900

    MODIFY: AccommodationListView Q 객체 필터링 추가 &  AccommodationDetailView unit test 완료

--- accommodation/tests.py
+++ accommodation/tests.py
@@ -15,16 +15,19 @@ from .models import (
     OrderStatus,
     AccommodationOrder,
     Review,
-    UnavailableDate
+    UnavailableDate,
+    RoomOption,
+    Option
 )
 
-from accommodation.views  import AccommodationListView
+from accommodation.views  import AccommodationListView, AccommodationDetailView
 
 client = Client()
 
 class AccommodationListTest(TestCase):
 
     def setUp(self):
+
         RefundAccount.objects.create(
             id                  = 1,
             bank_account_number = '01',
@@ -109,14 +112,22 @@ class AccommodationListTest(TestCase):
         )
 
         AccommodationOrder.objects.create(
+            id               = 1,
             total_price      = '333000.00',
             guest            = '2',
             start_date       = '2020-03-01',
             end_date         = '2020-03-15',
             serial_number    = 'ordernumber00001',
             order_status_id  = 1,
-            user_id = 1,
+            user_id          = 1,
             room_id          = 1
+        )
+
+        UnavailableDate.objects.create(
+            id         = 1,
+            start_date = '2020-09-09',
+            end_date   = '2020-10-09',
+            room_id    = 1
         )
 
         Review.objects.create(
@@ -130,13 +141,6 @@ class AccommodationListTest(TestCase):
             user_id                = 1,
             accommodation_id       = 1,
             accommodation_order_id = 1
-        )
-
-        UnavailableDate.objects.create(
-            id         = 1,
-            start_date = '2020-09-09',
-            end_date   = '2020-10-09',
-            room_id    = 1
         )
 
     def tearDown(self):
@@ -156,14 +160,179 @@ class AccommodationListTest(TestCase):
 
     def test_accommodation_list_data(self):
         response = client.get(f'/accommodation?city={self.city.name}&startDate=2021-03-17&endDate=2021-03-18&guest=2&order=favored')
-        
+
         self.assertEqual(response.status_code, 200)
 
+class AccommodationDetailTest(TestCase):
+    maxDiff=None
 
+    def setUp(self):
 
+        RefundAccount.objects.create(
+            id                  = 1,
+            bank_account_number = '01',
+            bank_name           = 'CITYBANK',
+            owner               =' Hee Young Jung'
+        )
+
+        User.objects.create(
+            id                = 1,
+            nickname          = '정희영',
+            email             = 'jhee@wecode.com',
+            image_url         = 'image01/wecode.com',
+            refund_account_id = 1
+        )
         
+        Address.objects.create(
+            id        = 1, 
+            name      = '제주특별자치도 제주시 한림읍 귀덕리 1836-3',
+            longitude = '123.00001', 
+            latitude  = '192.00001'
+        )
 
+        Host.objects.create(
+            id        = 1,
+            name      ='허민지', 
+            image_url ='null'
+        )
 
+        self.city = City.objects.create(
+            id   = 1, 
+            name = '제주'
+        )
 
+        Category.objects.create(
+            id   = 1,
+            name = '호텔'
+        )
         
+        accommodation = Accommodation.objects.create(
+            id          = 1,
+            name        = '토토 게스트하우스',
+            description = '파도를 감상하며 명상하기 좋은 게스트하우스', 
+            rate        = '4.20', 
+            content     = 'null', 
+            address_id  = 1, 
+            host_id     = 1, 
+            city_id     = 1, 
+            category_id = 1
+        )
 
+        Room.objects.create(
+                id               = 1,
+                name             = '스튜디오1',
+                price            = '89000.00',
+                maximum_capacity = '3',
+                basic_capacity   = '2',
+                accommodation_id = 1,
+                extra_price      = '0'
+        )
+
+        AccommodationImage.objects.create(
+            image_url        = 'image01/wecode.com',
+            accommodation_id = 1,
+            room_id          = 1
+        )
+
+        OrderStatus.objects.create(
+            id   = 1,
+            name = '예약완료'
+        )
+
+        AccommodationOrder.objects.create(
+            id               = 1,
+            total_price      = '333000.00',
+            guest            = '2',
+            start_date       = '2020-03-01',
+            end_date         = '2020-03-15',
+            serial_number    = 'ordernumber00001',
+            order_status_id  = 1,
+            user_id          = 1,
+            room_id          = 1
+        )
+
+        UnavailableDate.objects.create(
+            id         = 1,
+            start_date = '2020-09-09',
+            end_date   = '2020-10-09',
+            room_id    = 1
+        )
+
+        Review.objects.create(
+            content                = '우리 팀 멋있다!',
+            image_url              = 'www.wecode.com',
+            location_rate          = '3.4',
+            sanitary_rate          = '4.5',
+            facility_rate          = '3.2',
+            hospitality_rate       = '128.00001',
+            total_rate             = '128.00002',
+            user_id                = 1,
+            accommodation_id       = 1,
+            accommodation_order_id = 1
+        )
+
+        Option.objects.create(
+            id   = 1,
+            name = 'WIFI'
+        )
+
+        room_option = RoomOption.objects.create(
+            id        = 1,
+            quantity  = '1',
+            option_id = 1,
+            room_id   = 1
+        )
+
+    def tearDown(self):
+        Accommodation.objects.all().delete()
+        User.objects.all().delete()
+        Address.objects.all().delete()
+        Host.objects.all().delete()
+        City.objects.all().delete()
+        Category.objects.all().delete()
+        Room.objects.all().delete()
+        AccommodationImage.objects.all().delete()
+        OrderStatus.objects.all().delete()
+        AccommodationOrder.objects.all().delete()
+        Review.objects.all().delete()
+        RefundAccount.objects.all().delete()
+        UnavailableDate.objects.all().delete()
+        RoomOption.objects.all().delete()
+
+    def test_accommodation_list_detail_success(self): 
+        accommodation_id = 1
+        response         = client.get(f'/accommodation/{accommodation_id}')
+
+        data = {
+            'id'            : 1,
+            'name'          : '토토 게스트하우스',
+            'description'   : '파도를 감상하며 명상하기 좋은 게스트하우스',
+            'total_rate'    : '4.20',
+            'review_number' : 1,
+            'room'          : [{
+                'name'           : '스튜디오1',
+                'option'         : ['WIFI'],
+                'price'          : '89000.00',
+                'basic_capacity' : 2,
+                'max_capacity'   : 3,
+                'extra_price'    : '0.00',
+                'image'          : ['image01/wecode.com'],
+            }],
+            'main_image'    : 'image01/wecode.com',
+            'address'       : '제주특별자치도 제주시 한림읍 귀덕리 1836-3',
+            'host'          : {
+                'name'           : '허민지',
+                'image_url'      : 'null'
+            },
+            'content'       : 'null'
+        }
+
+        self.assertEqual(response.json(), {'data': data})
+        self.assertEqual(response.status_code, 200)
+
+    def test_accommodation_detail_fail(self):
+        accommodation_id = 2
+        response         = client.get(f'/accommodation/{accommodation_id}')
+
+        self.assertEqual(response.json(), {'message': 'INVALID_ACCOMMODATION_ID'})
+        self.assertEqual(response.status_code, 400)
--- accommodation/views.py
+++ accommodation/views.py
@@ -39,49 +39,38 @@ class AccommodationListView(View):
     rate=0
     """
 
-    def get(self, request, ):
-        city         = request.GET.get('city', None)
-        start_date   = request.GET.get('startDate', None)
-        end_date     = request.GET.get('endDate', None)
-        guest        = request.GET.get('guest', None)
+    def get(self, request, category_id=0):
+        city         = request.GET['city']
+        start_date   = request.GET['startDate']
+        end_date     = request.GET['endDate']
+        guest        = request.GET['guest']
         ordering     = request.GET.get('order', 'favored')
-        rate         = request.GET.get('rate', 0)
-        category     = request.GET.getlist('category', '호텔')
+        rate         = request.GET.get('rate', 0.00)
         room_option  = request.GET.getlist('roomOption', None)
 
-        condition  = (
-                Q(city__name= city)
-                & Q(room__maximum_capacity__gte=guest)
-                & ~Q(room__unavailabledate__start_date__gte=start_date, room__unavailabledate__end_date__lte=end_date)
+        q = (Q(city__name=city) 
+            & Q(room__maximum_capacity__gte=guest) 
+            & Q(rate__gte=rate) 
+            & ~Q(room__unavailabledate__start_date__gte=start_date, room__unavailabledate__end_date__lte=end_date)
         )
-        
+
+        if category_id:
+            q &= Q(category__id=category_id)
+
+        if room_option:
+            q &= Q(room__option__name__in=room_option)
+
         accommodations = Accommodation.objects\
-            .select_related(
-            'address',
-            'category',
-            'city')\
-            .prefetch_related(
-            'accommodationimage_set',
-            'room_set',
-            'review_set')\
-            .filter(condition)\
+            .select_related('address','category','city')\
+            .prefetch_related('accommodationimage_set','room_set','review_set')\
+            .filter(q)\
             .annotate(count=Count('review'), price=Min('room__price'))
 
         ordering_options = {
             'favored' : '-count',
             'ratingHigh' : '-rate',
-            'priceLow' : 'room__price',
-            'priceHigh' : '-room__price'
-        }
-
-        filter_prefixes = {
-            'category'    : 'category__name__in',
-            'room_option' : 'room__option__name__in',
-            'rate'        : 'rate__gte'
-        }
-
-        filter_set = {
-            filter_prefixes.get(key): value for (key, value) in dict(request.GET).items() if filter_prefixes.get(key)
+            'priceLow' : 'price',
+            'priceHigh' : '-price'
         }
 
         data = [
@@ -93,8 +82,61 @@ class AccommodationListView(View):
                 'rate'        : accommodation.rate,
                 'review'      : accommodation.count,
                 'price'       : accommodation.price,
-                'url'         : accommodation.accommodationimage_set.first().image_url,
-            } for accommodation in accommodations.filter(**filter_set).order_by(ordering_options[ordering])
+                'url'         : accommodation.accommodationimage_set.all()[0].image_url,
+            } for accommodation in accommodations.order_by(ordering_options[ordering]) 
         ]
 
         return JsonResponse({'data': data}, status=200)
+
+class AccommodationDetailView(View):
+    def get(self, request, accommodation_id):
+        try:
+            accommodation = Accommodation.objects.get(id=accommodation_id)
+            review_count  = Review.objects.filter(accommodation=accommodation_id).count()
+            
+            data = {
+                'id'            : accommodation.id,
+                'name'          : accommodation.name,
+                'description'   : accommodation.description,
+                'total_rate'    : accommodation.rate,
+                'review_number' : review_count,
+                'room'          : [{
+                    'name'           : room.name,
+                    'option'         : [
+                        {
+                            'name' : option.name,
+                            'quantity': option.roomoption_set.filter(room=room.id)[0].quantity
+                        } for option in room.option.all()
+                    ],
+                    'price'          : room.price,
+                    'basic_capacity' : room.basic_capacity,
+                    'max_capacity'   : room.maximum_capacity,
+                    'extra_price'    : room.extra_price,
+                    'image'          : [
+                        {
+                            'image_url' : image.image_url 
+                        } for image in room.accommodation.accommodationimage_set.all()
+                    ],
+                } for room in accommodation.room_set.all()],
+                'main_image'    : accommodation.accommodationimage_set.first().image_url,
+                'address'       : accommodation.address.name,
+                'host'          : {
+                    'name'           : accommodation.host.name,
+                    'image_url'      : accommodation.host.image_url
+                },
+                'content'       : accommodation.content,
+                'review_list'   : [
+                    {
+                        'user'        : review.user.nickname,
+                        'created_at'  : review.created_at,
+                        'content'     : review.content,
+                        'image_url'   : review.image_url,
+                        'total_rate'  : review.total_rate
+                    } for review in accommodation.review_set.all()
+                ]
+            }
+
+            return JsonResponse({'data': data}, status=200)
+
+        except Accommodation.DoesNotExist:
+            return JsonResponse({'message': 'INVALID_ACCOMMODATION_ID'}, status=400)
