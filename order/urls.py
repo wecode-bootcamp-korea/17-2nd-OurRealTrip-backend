b8cdc5db36c2c163ca45634e403949d99c4709c2
sayxyoung <hyjlove11@gmail.com>
2021-03-11 21:16:42 +0900

    ADD: 숙소예약 기능구현

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from order.views import FlightRoundTripOrderView
+from order.views import FlightRoundTripOrderView, AccommodationOrderView
 
 urlpatterns = [
     path('/flight', FlightRoundTripOrderView.as_view()),
+    path('/accommodation', AccommodationOrderView.as_view()),
 ]
 
--- order/views.py
+++ order/views.py
@@ -7,7 +7,8 @@ from my_settings    import SECRET_KEY, ALGORITHM
 from user.utils     import validate_signin
 from user.models    import User
 from flight.models  import FlightPrice
-from order.models   import OrderStatus, FlightOrder
+from order.models   import OrderStatus, FlightOrder, AccommodationOrder
+from accommodation.models import Room
 
 class FlightRoundTripOrderView(View):
     @validate_signin
@@ -38,3 +39,32 @@ class FlightRoundTripOrderView(View):
         except FlightPrice.DoesNotExist:
             return JsonResponse({'message' : 'INVALID_FLIGHT_ID'}, status=400)
 
+class AccommodationOrderView(View):
+    @validate_signin
+    def post(self, request):
+        try:
+            user        = request.user
+            data        = json.loads(request.body)
+            total_price = data.get('totalPrice')
+            guest       = data.get('guest')
+            start_date  = data.get('startDate')
+            end_date    = data.get('endDate')
+            room        = data.get('room')
+
+            AccommodationOrder.objects.create(
+                total_price   = total_price,
+                guest         = guest,
+                start_date    = start_date,
+                end_date      = end_date,
+                serial_number = uuid.uuid4(),
+                room          = Room.objects.get(id=room),
+                order_status  = OrderStatus.objects.get(name="결제 대기"),
+                user          = user,
+            )
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
+
+        except Room.DoesNotExist:
+            return JsonResponse({'message': 'ROOM_DOES_NOT_EXIST'}, status=400)
+
+        except OrderStatus.DoesNotExist:
+            return JsonResponse({'message': 'ORDER_STATUS_DOES_NOT_EXIST'}, status=400)
